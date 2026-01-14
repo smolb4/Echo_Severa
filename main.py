b755a1783068d1162702d94b4525d3ed3a27936d
smolb4 <145501808+smolb4@users.noreply.github.com>
2026-01-14 12:34:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,30 @@ import random
 
 app = FastAPI()
 TOKEN = "vk1.a.sq5rMHr7_eVlqS9xPvZKC2faTUGZBGT0EeXkSYGIpw1dAe0a6Rrw_hHUSsicD21cRLAUcGd_hzA_BLd0R37aOa7fGCf9vpPkUwRT9uOJlSiMQHCZz397zimUVgVZz9jgV_OOv5vmX6I5aoRAMfCMm0NEgxMd9UgmFgISq_krk2fBhaWC5S6wjvki3apnVH19xScFwNFkOUELvD0DPJQNyA"
-GROUP_ID = 235128907
+GROUP_ID = -235128907
 
 @app.post("/form")
 async def form(requests: Request):
-    data = await requests.json()
-    
-    name = data.get("Имя")
-    gender = data.get("Пол")
-    age = data.get("Возраст")
-    country = data.get("Происхождение")
-    role = data.get("Позиция")
-    body = data.get("Телосложение")
-    height = data.get("Рост")
-    eye = data.get("Глаза")
-    wool = data.get("Шерсть")
-    ref = data.get("Ссылка на реф")
-    look = data.get("Внешность")
-    character = data.get("Характер")
-    character2 = data.get("Характер подробнее")
-    goals = data.get("Цели")
-    skills = data.get("Навыки")
+    try:
+        data = await requests.json()
+    except:
+        return {"error": "Некорректный JSON"}
+
+    name = data.get("Имя", "")
+    gender = data.get("Пол", "")
+    age = data.get("Возраст", "")
+    country = data.get("Происхождение", "")
+    role = data.get("Позиция", "")
+    body = data.get("Телосложение", "")
+    height = data.get("Рост", "")
+    eye = data.get("Глаза", "")
+    wool = data.get("Шерсть", "")
+    ref = data.get("Ссылка на реф", "")
+    look = data.get("Внешность", "")
+    character = data.get("Характер", "")
+    character2 = data.get("Характер подробнее", "")
+    goals = data.get("Цели", "")
+    skills = data.get("Навыки", "")
 
     text = f"""Новая заявка
 Имя: {name}
@@ -43,15 +46,18 @@ async def form(requests: Request):
 Цели: {goals}
 Навыки: {skills}
 """
-    requests.post(
-        "https://api.vk.com/method/messages.send",
-        data={
-            "peer_id": 388182166,
-            "message": text,
-            "random_id": random.randint(1, 10**9),
-            "access_token": TOKEN,
-            "v": "5.131"
-        }
-    )
+    try:
+        requests.post(
+            "https://api.vk.com/method/messages.send",
+            data={
+                "peer_id": 388182166,
+                "message": text,
+                "random_id": random.randint(1, 10**9),
+                "access_token": TOKEN,
+                "v": "5.131"
+            }
+        )
+    except Exception as e:
+        return {"error": str(e)}
 
     return {"ok": True}
